db2995cf6f52f13bc9e2c4976d7a403601bf3062
Adrián Sigüenza Ramírez <88464152+Adrisira@users.noreply.github.com>
2022-07-25 02:24:44 +0200

    4.4

---
+++ Part 4/Programs/Mutable&Inmutable.py
@@ -0,0 +1,10 @@
+def nombre():
+    name = 'Zophie a cat'
+    newName = name[0:7] + 'the' + name[8:12]
+    print(name)
+    print(newName)
+    
+nombre()
+# Manera de monstar una lista inmutable de la forma que queremos
+# Recuerda siempre que si declaro una variable con el mimso nombre, siempre se impone la ultima
+
